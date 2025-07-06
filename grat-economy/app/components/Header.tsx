0acf3691c5ffabe3c48296d7611b9825cc75a81a
Moses-main <moseschizaram8@gmail.com>
2025-07-06 05:40:10 +0100

    remove the mobile navigation

--- grat-economy/app/components/Header.tsx
+++ grat-economy/app/components/Header.tsx
@@ -33,8 +33,7 @@ export default function Header() {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
+              {/* <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-black focus:outline-none focus:ring-1 focus:ring-inset focus:rounded-none focus:ring-black">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -43,7 +42,7 @@ export default function Header() {
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
-              </div>
+              </div> */}
               {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <Image
